ae9c1609f3b80104beee64130a5142822cedfb91
TWR-Dominik <Dominik.hobel2001@gmail.com>
2023-03-04 12:48:22 +0100

    Created connection to tcp Server

--- app/src/main/java/com/example/se2_ue1/MainActivity.kt
+++ app/src/main/java/com/example/se2_ue1/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.se2_ue1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.se2_ue1.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.run {
+            button.setOnClickListener{ sendInput() }
+        }
     }
 
+    private fun sendInput(){
+        Thread{
+            try {
+                val matrikelnummer = binding.matrikelnummer.text.toString()
+                val socket = Socket("se2-isys.aau.at",53212)
+                val toServer = socket.getOutputStream()
+                val fromServer = socket.getInputStream()
+
+                toServer.write((matrikelnummer + "\n").toByteArray())
+
+                val responseBuffer = ByteArray(1024)
+                val bytesRead = fromServer.read(responseBuffer)
+                val response = responseBuffer.copyOfRange(0, bytesRead).toString(Charsets.UTF_8)
+
+                socket.close()
+
+                runOnUiThread {
+                    binding.textView2.text = response
+                }
+            }catch (e: IOException) {
+                Log.d("MainActivity", "Error")
+                e.printStackTrace()
+            }
+        }.start()
+
+    }
 }
